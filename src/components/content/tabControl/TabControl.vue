9cd3bb9efeace7351bf6108aee20926b4d674a75
simin-xxm <Simen313523658@gmail.com>
2020-09-19 17:12:08 +0800

    update

--- src/components/content/tabControl/TabControl.vue
+++ src/components/content/tabControl/TabControl.vue
@@ -1,6 +1,8 @@
+
+
 <template>
   <div class="tab-control">
-    <div v-for="item in titles" class="tab-control-item">
+    <div v-for="(item,index) in titles" class="tab-control-item" :class="{active: index===currentIndex}" @click="itemClick(index)">
       <span>{{item}}</span>
     </div>
   </div>
@@ -16,6 +18,16 @@
           return []
         }
       }
+    },
+    data() {
+      return {
+        currentIndex: 0
+      }
+    },
+    methods: {
+      itemClick(index){
+        this.currentIndex = index
+      }
     }
   }
 </script>
@@ -28,5 +40,12 @@
     line-height: 40px;
     font-size: 15px;
     background-color: #f5f5f5;
+  }
+  .tab-control-item span{
+    padding: 5px;
+  }
+  .active span{
+    color: var(--color-high-text);
+    border-bottom: 3px solid var(--color-tint);
   }
 </style>
